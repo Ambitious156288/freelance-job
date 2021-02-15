8be977c91f283d95f01be337306d6b3f40696d5d
Krystian Kalwik <ambitious156288@gmail.com>
2021-02-16 00:21:20 +0100

    about page done

--- src/views/Root/About.js
+++ src/views/Root/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import shoemaker from 'assets/icons/shoemaker.jpg';
+import Fonts from 'components/atoms/Fonts/Fonts';
 
 const StyledView = styled.div`
   position: relative;
@@ -15,10 +16,33 @@ const StyledBgc = styled.div`
 
 const StyledDiv = styled.div`
   position: absolute;
+  text-align: center;
   left: 0;
   height: 100vh;
   width: 50vw;
   background-color: ${({ theme }) => theme.black};
+`;
+
+const StyledFirst = styled(Fonts)`
+  font-size: 89px;
+  font-weight: bold;
+  font-style: italic;
+  color: ${({ theme }) => theme.orange};
+`;
+
+const StyledSecond = styled(Fonts)`
+  font-size: 28px;
+  font-weight: 300;
+  text-align: left;
+  margin: 90px 0 0 150px;
+`;
+
+const StyledCenter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 100px 50px;
 `;
 
 const About = () => (
@@ -28,12 +52,15 @@ const About = () => (
     </StyledBgc>
 
     <StyledDiv>
-      <title>Kim jesteśmy?</title>
-      <p>
-        Jako firma specjalizujemy się przede wszystkim w naprawie obuwia. Naprawiamy obuwie
-        wizytowe, sportowe oraz luksusowe !! Doświadczenie zawodowe pozwala nam na wykonanie wielu
-        usług na najwyższym poziomie estetycznym w sposób bardzo tradycyjny i profesjonalny !!
-      </p>
+      <StyledCenter>
+        <StyledFirst Playfair>Kim jesteśmy?</StyledFirst>
+        <StyledSecond>
+          Jako firma specjalizujemy się przede wszystkim w naprawie obuwia. Naprawiamy obuwie
+          wizytowe, sportowe oraz luksusowe !!
+          <br /> Doświadczenie zawodowe pozwala nam na wykonanie wielu usług na najwyższym poziomie
+          estetycznym w sposób bardzo tradycyjny i profesjonalny !!
+        </StyledSecond>
+      </StyledCenter>
     </StyledDiv>
   </StyledView>
 );
