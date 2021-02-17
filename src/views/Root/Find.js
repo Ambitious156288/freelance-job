35d4dbdf6f5314edd7527cc13f6775b3762fe1cc
Krystian Kalwik <ambitious156288@gmail.com>
2021-02-17 04:38:27 +0100

    find subpage done

--- src/views/Root/Find.js
+++ src/views/Root/Find.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import Fonts from 'components/atoms/Fonts/Fonts';
 import AppMap from '../../data/AppMap';
+
+const StyledTitle = styled(Fonts)`
+  font-size: 70px;
+  font-weight: bold;
+`;
+
+const StyledDiv = styled(Fonts)`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.red};
+`;
 
 const Find = () => {
   return (
     <>
-      <div id="find">Znajdz nas</div>
-      <AppMap />
+      <div id="find">
+        <StyledDiv>
+          <StyledTitle Playfair>Zapraszamy do punktów przyjęć</StyledTitle>
+        </StyledDiv>
+
+        <AppMap />
+      </div>
     </>
   );
 };
